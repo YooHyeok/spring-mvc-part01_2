2bf6562d13c51879339dfc6e78df3f23419ed41b
YooHyeok <yjk7454@naver.com>
2023-03-29 00:21:12 +0900

    요청 매핑 API 예시 - GET/POST/PATCH/DELETE

---
+++ src/main/java/hello/spring/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,36 @@
+package hello.spring.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * GET, POST, PATCH, DELETE
+ * 간단한 API 구현
+ */
+@RestController
+public class MappingClassController {
+
+    @GetMapping("/mapping/users")
+    public String users() {
+        return "get users";
+    }
+
+    @PostMapping("/mapping/users")
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/mapping/users/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId = " + userId;
+    }
+
+    @PatchMapping("/mapping/users/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId = " + userId;
+    }
+
+    @DeleteMapping("/mapping/users/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId = " + userId;
+    }
+}
