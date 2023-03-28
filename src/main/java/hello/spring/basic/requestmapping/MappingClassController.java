f0384d4ce4473ab988b54541603f322abe71c093
YooHyeok <yjk7454@naver.com>
2023-03-29 00:26:16 +0900

    요청 매핑 API 예시 - 공통URI 매핑주소 설정- 클래스 레벨에 선언

--- src/main/java/hello/spring/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/spring/basic/requestmapping/MappingClassController.java
@@ -7,29 +7,30 @@ import org.springframework.web.bind.annotation.*;
  * 간단한 API 구현
  */
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String users() {
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser() {
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("{userId}")
     public String findUser(@PathVariable String userId) {
         return "get userId = " + userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("{userId}")
     public String updateUser(@PathVariable String userId) {
         return "update userId = " + userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete userId = " + userId;
     }
