ce44875418463d2e0fd1b9722dda8815f7046ec1
YooHyeok <yjk7454@naver.com>
2023-03-30 00:35:55 +0900

    BasicItemController 추가 - items() 메소드 추가

---
+++ src/main/java/hello/spring/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,28 @@
+package hello.spring.itemservice.web.basic;
+
+import hello.spring.itemservice.domain.item.Item;
+import hello.spring.itemservice.domain.item.ItemRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/item";
+    }
+}
