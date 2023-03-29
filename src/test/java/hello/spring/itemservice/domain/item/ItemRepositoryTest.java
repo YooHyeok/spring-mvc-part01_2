bc167228e49c142b7e80c3c72343135293f26955
YooHyeok <yjk7454@naver.com>
2023-03-29 23:57:44 +0900

    itemRepositoryTest - findAll 전체조회 테스트 메서드 추가

--- src/test/java/hello/spring/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/spring/itemservice/domain/item/ItemRepositoryTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ItemRepositoryTest {
@@ -26,8 +28,22 @@ class ItemRepositoryTest {
         Item findItem = itemRepository.findById(item.getId());
 
         assertThat(findItem).isEqualTo(savedItem);
-
-
     }
 
+    @Test
+    void findAll() {
+        //given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 10000, 10);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        //when
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2); // result의 크기가 2 인가.
+        assertThat(result).contains(item1, item2); // result안에 item1과 item2 객체가 각각 포함되어있는가
+    }
 }
