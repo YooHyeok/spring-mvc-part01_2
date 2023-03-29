56706bcb3a64bf84d88f2bfde1ad3bfb67bb4f21
YooHyeok <yjk7454@naver.com>
2023-03-30 01:43:42 +0900

    ItemController에 상품 단건조회 매핑 메소드 추가

--- src/main/java/hello/spring/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/spring/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -19,11 +20,29 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
+    /**
+     * 상품 목록
+     * @param model
+     * @return
+     */
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    /**
+     * 상품 단건 조회
+     * @param itemId
+     * @param model
+     * @return
+     */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+            return "basic/item";
     }
 
     /**
