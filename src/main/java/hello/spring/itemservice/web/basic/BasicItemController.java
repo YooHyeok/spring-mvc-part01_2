50c515c7f3c57aca1b520dfb5afcee7811c2bd7d
YooHyeok <yjk7454@naver.com>
2023-03-30 02:10:40 +0900

    ItemController 상품등록 add() 메소드 추가 및 addForm.html파일 추가

--- src/main/java/hello/spring/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/spring/itemservice/web/basic/BasicItemController.java
@@ -45,6 +45,11 @@ public class BasicItemController {
             return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
