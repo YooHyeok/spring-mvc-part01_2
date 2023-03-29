0e7444ae0797a2ba78497a15e103ba633455251b
YooHyeok <yjk7454@naver.com>
2023-03-29 23:33:59 +0900

    itemRepository - 상품 단건 조회 로직 추가

--- src/main/java/hello/spring/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/spring/itemservice/domain/item/ItemRepository.java
@@ -2,7 +2,9 @@ package hello.spring.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -13,11 +15,20 @@ public class ItemRepository {
     /**
      * 상품 저장
      * @param item
-     * @return
+     * @return 상품정보(item)
      */
     public Item save(Item item) {
         item.setId(++sequence);
         store.put(item.getId(), item);
         return item;
+    }
+
+    /**
+     * 상품 단건 조회
+     * @param id
+     * @return store.get(id) (id로 store로부터 검색)
+     */
+    public Item findById(Long id) {
+        return store.get(id);
     }
 }
