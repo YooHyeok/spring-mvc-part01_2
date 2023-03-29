a7a470352ae4f0fd16ccd36ebf161b27ce65085e
YooHyeok <yjk7454@naver.com>
2023-03-29 23:27:39 +0900

    item 도메인 item(vo)클래스 생성

---
+++ src/main/java/hello/spring/itemservice/domain/item/Item.java
@@ -0,0 +1,21 @@
+package hello.spring.itemservice.domain.item;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(Long id, String itemName, Integer price, Integer quantity) {
+        this.id = id;
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
